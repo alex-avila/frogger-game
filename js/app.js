0a1ce7dc40b290bb36994f780553d8b48d7b22d3
alex-avila <alex.avilx@icloud.com>
2018-08-08 21:06:18 -0600

    Add more modal behaviour and score behaviour
    Can't play game when modal is active and score is based on time & moves.

--- js/app.js
+++ js/app.js
@@ -4,18 +4,41 @@ const randomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) 
 const render = (sprite, x, y) => ctx.drawImage(Resources.get(sprite), x, y);
 const yCenter = (coord) => 83 * (coord - 1/2);
 const xCenter = (coord) => coord * 100;
+const getScore = (moves, time) => Math.round(moves / time / 2 * 500);
 
-// DOM elements
+// DOM variables
 const modal = document.getElementById('modal');
 const scoreDisplay = document.getElementById('modal__score');
 const playBtn = document.getElementById('modal__btn');
+let score = 0;
+let isModalShown = false;
 
 // DOM manipulation
-const toggleModal = () => modal.classList.toggle('visible')
-playBtn.addEventListener('click', toggleModal)
-modal.addEventListener('click', e => e.target.id === 'modal' ? toggleModal() : null)
+// Updates score at 60 frames per second
+const displayScore = score => {
+    // 1000 / 60 = 16.66667 (milliseconds per frame)
+    const milliPerFrame = 16.66667;
+    const duration = 1250;
+    const increment = score / (duration / milliPerFrame);
+    scoreDisplay.textContent = 0;
+    const interval = setInterval(() => {
+        let newNum = Math.round(parseInt(scoreDisplay.textContent) + increment)
+        newNum = newNum > score ? score : newNum
+        scoreDisplay.textContent = newNum
+        if (newNum === score) clearInterval(interval)
+    }, milliPerFrame);
+}
 
-// Parent Class
+const toggleModal = () => {
+    modal.classList.toggle('visible');
+    setTimeout(() => displayScore(score), 250);
+    isModalShown = !isModalShown;
+};
+
+playBtn.addEventListener('click', toggleModal);
+modal.addEventListener('click', e => e.target.id === 'modal' ? toggleModal() : null);
+
+// Parent Class;
 class PositionedThing {
     constructor(sprite, x, y) {
         this.x = x;
@@ -55,6 +78,7 @@ class Enemy extends PositionedThing {
             && this.x > player.x - 40
             && this.y === player.y
         ) {
+            score -= 500;
             const { x, y } = player.initialCoordinates;
             player.update(x, y);
         }
@@ -85,9 +109,17 @@ class Player extends PositionedThing {
             right: xCenter(5),
             win: yCenter(0)
         };
+        this.moves = 0;
+        this.start = null;
+        this.end = null;
     }
 
     reset() {
+        this.end = Date.now();
+        score += getScore(this.moves, (this.end - this.start) / 1000);
+
+        this.moves = 0;
+        this.start = null;
         const { x, y } = this.initialCoordinates;
         this.update(x, y);
     }
@@ -104,7 +136,10 @@ class Player extends PositionedThing {
     }
 
     handleInput(direction) {
-        const { boundaries } = this
+        if (isModalShown) return;
+        const { boundaries } = this;
+        if (direction) this.moves++;
+        if (this.moves === 1) this.start = Date.now();
         switch (direction) {
             case 'up':
                 if (this.y -83 === boundaries.win) {
